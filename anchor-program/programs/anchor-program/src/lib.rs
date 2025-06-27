97dea62ae5d07f757607ce547406734cba6827f6
0xSunill <sunday7637@gmail.com>
2025-06-27 04:53:42 +0000

    new

--- anchor-program/programs/anchor-program/src/lib.rs
+++ anchor-program/programs/anchor-program/src/lib.rs
@@ -7,8 +7,7 @@ pub mod anchor_program {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+       
     }
 }
 

71eb5483fa9d10fa2d049a7bddde154436239ae2
0xSunill <sunday7637@gmail.com>
2025-06-27 06:58:55 +0000

    final contract

--- anchor-program/programs/anchor-program/src/lib.rs
+++ anchor-program/programs/anchor-program/src/lib.rs
@@ -4,18 +4,34 @@ declare_id!("3VCvp1YPmgXYxafYx9kvCDm2DcoufXuhd8j7P7E861Qq");
 
 #[program]
 pub mod anchor_program {
-    use core::num;
     use super::*;
+    use core::num;
     pub fn initialize(ctx: Context<Initialize>, inititial_value: u32) -> Result<()> {
         ctx.accounts.account.numb = inititial_value;
+        Ok(())
+    }
+
+    pub fn double(ctx: Context<Double>) -> Result<()> {
+        ctx.accounts.account.numb = ctx.accounts.account.numb * 2;
         Ok(())
     }
     pub fn add(ctx: Context<Add>, num: u32) -> Result<()> {
         ctx.accounts.account.numb = ctx.accounts.account.numb + num;
         Ok(())
     }
-    pub fn double(ctx: Context<Double>) -> Result<()> {
-        ctx.accounts.account.numb = ctx.accounts.account.numb * 2;
+
+    pub fn sub(ctx: Context<Subtract>, num: u32) -> Result<()> {
+        ctx.accounts.account.numb = ctx.accounts.account.numb - num;
+        Ok(())
+    }
+
+    pub fn mul(ctx: Context<Multiply>, num: u32) -> Result<()> {
+        ctx.accounts.account.numb = ctx.accounts.account.numb * num;
+        Ok(())
+    }
+
+    pub fn div(ctx: Context<Devide>, num: u32) -> Result<()> {
+        ctx.accounts.account.numb = ctx.accounts.account.numb / num;
         Ok(())
     }
 }
@@ -35,6 +51,15 @@ pub struct Initialize<'info> {
 }
 
 #[derive(Accounts)]
+pub struct Double<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
 pub struct Add<'info> {
     #[account(mut)]
     pub account: Account<'info, DataShape>,
@@ -44,7 +69,25 @@ pub struct Add<'info> {
 }
 
 #[derive(Accounts)]
-pub struct Double<'info> {
+pub struct Subtract<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Multiply<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Devide<'info> {
     #[account(mut)]
     pub account: Account<'info, DataShape>,
 

d9accb5ed4c63d0099b95728794853f5ccc8f3b1
0xSunill <sunday7637@gmail.com>
2025-06-27 05:59:26 +0000

    anchor strucutre

--- anchor-program/programs/anchor-program/src/lib.rs
+++ anchor-program/programs/anchor-program/src/lib.rs
@@ -4,12 +4,49 @@ declare_id!("3VCvp1YPmgXYxafYx9kvCDm2DcoufXuhd8j7P7E861Qq");
 
 #[program]
 pub mod anchor_program {
+    use core::num;
+
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-       
+        Ok(())
+    }
+    pub fn add(ctx: Context<Add>, num: u32) -> Result<()> {
+        Ok(())
+    }
+    pub fn double(ctx: Context<Double>) -> Result<()> {
+        Ok(())
     }
 }
 
+#[account]
+struct DataShape {
+    pub num: u32,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init,payer= signer, space =8 + 4)]
+    pub account: Account<'info, DataShape>,
+    pub system_program: Program<'info, System>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Add<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Double<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
